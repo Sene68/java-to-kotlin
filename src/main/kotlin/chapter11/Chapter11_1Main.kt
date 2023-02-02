5a083d751d10c6031cc258b1c3b9f31fba14f8d8
Sene68 <senefull68@gmail.com>
2023-02-02 14:44:22 +0900

    chapter11 - packages

---
+++ src/main/kotlin/chapter11/Chapter11_1Main.kt
@@ -0,0 +1,16 @@
+package chapter11
+
+fun main() {
+    chapter11()
+    println(level)
+    println(hp)
+}
+
+private fun chapter11() {
+    println("This chapter is 11")
+}
+
+public var level: Int = 5
+    private set
+
+internal val hp = 100
