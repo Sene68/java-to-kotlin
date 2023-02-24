e2a956125c7c169ff896209d73e483cb0c4a6dff
Sene68 <senefull68@gmail.com>
2023-02-24 20:32:20 +0900

    chapter18 - filter & map

---
+++ src/main/kotlin/chapter18/Chapter18_1Main.kt
@@ -0,0 +1,18 @@
+package chapter18
+
+fun main() {
+    val graphicsCards = listOf(
+        GraphicsCard("Geforce GTX 1060", 230000),
+        GraphicsCard("Radeon 5300", 660000),
+    )
+
+    val radeonPrices = graphicsCards.filter { graphicsCard -> graphicsCard.name == "Radeon 5300" }
+        .map { graphicsCard -> graphicsCard.price}
+
+    println(radeonPrices)
+}
+
+data class GraphicsCard (
+    val name: String,
+    val price: Long,
+)
