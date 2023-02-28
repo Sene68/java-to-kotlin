939925042bd27878c18f2a03e8fbcecaced9cfa3
Sene68 <senefull68@gmail.com>
2023-02-28 09:43:23 +0900

    chapter18 - flatmap

---
+++ src/main/kotlin/chapter18/Chapter18_3Main.kt
@@ -0,0 +1,24 @@
+package chapter18
+
+fun main() {
+    val graphicsCards = listOf(
+        listOf(
+            GraphicsCard(0, "Geforce GTX 1060", 230000),
+            GraphicsCard(1, "Radeon 5300", 660000),
+        ),
+        listOf(
+            GraphicsCard(2, "Geforce GTX 2070", 430000),
+            GraphicsCard(3, "Radeon 3300", 160000),
+        ),
+        listOf(
+            GraphicsCard(4, "Geforce GTX 3080", 1430000),
+            GraphicsCard(5, "Radeon 4300", 360000),
+        )
+    )
+
+    val radeonGraphicsCards = graphicsCards.flatMap { list ->
+        list.filter { graphicsCard -> graphicsCard.name.contains("Radeon") }
+    }
+
+    println(radeonGraphicsCards)
+}
